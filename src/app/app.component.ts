fce7de79772bbc69339f44ecdea2505745b388bd
narendram224 <narendram224.nm@gmail.com>
2019-11-27 22:53:13 +0530

    now its time to use cicd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angularjenkins';
+  title = 'First Angualar jenkins projec';
 }
